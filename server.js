c5ca6bf3350ed27b63157e4f3e770856b15c8514
Sergey Meteor <sergey.n.sukhanov@gmail.com>
2016-05-04 20:46:04 +0300

    plug db click

--- server.js
+++ server.js
@@ -9,17 +9,17 @@
 
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser');
-var router = require('./api/router');
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test');
+//var bodyParser = require('body-parser');
+//var router = require('./api/router');
+//var mongoose = require('mongoose');
+//mongoose.connect('mongodb://localhost/test');
 
 var port = 8080;
 
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({extended:true}));
+//app.use(bodyParser.json());
 app.use(express.static('webapp'));
-app.use('/api', router);
+//app.use('/api', router);
 
 app.get('/', function(req, res){
     res.send(null);
