1d198fd0b039aff2b2b77591bc982ceebb53024f
Sergey Meteor <sergey.n.sukhanov@gmail.com>
2016-06-01 10:54:27 +0300

    update dashboard

--- server.js
+++ server.js
@@ -9,20 +9,8 @@
 
 var express = require('express');
 var app = express();
-//var bodyParser = require('body-parser');
-//var router = require('./api/router');
-//var mongoose = require('mongoose');
-//mongoose.connect('mongodb://localhost/test');
 
-var port = 8080;
-
-//app.use(bodyParser.urlencoded({extended:true}));
-//app.use(bodyParser.json());
-app.use(express.static('webapp'));
-
-app.get('/', function(req, res){
-    res.send(null);
-});
+app.use("/", express.static(__dirname + '/webapp'));
 
 app.listen(1234, function(){
     console.log("http://localhost:1234");
